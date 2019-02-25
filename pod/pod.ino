54f9c2889077221a0ec0dfe7868fae179751aa5f
Matthew Healy <mhealy@mst.edu>
2019-02-25 11:44:15 -0600

    Remove comments and extra variable

--- pod/pod.ino
+++ pod/pod.ino
@@ -7,18 +7,14 @@ const int HUMID_PIN  = 0;
 const int LIGHT_PIN  = 0;
 const char* ssid     = "GroMet Network";
 const char* passwd   = "wallace";
-//const IPAddress host = "GroMetHub.local";   //IPAdress type cannot be String
 const IPAddress host = (192, 168, 0, 1);
 const int    port    = 80;
 const String ID      = WiFi.macAddress();
-
-char* data;
 
 WiFiUDP udp;
 byte serdata    = 0;
 byte fromserver = 0;
 char json[SIZE];
-//String json = "";
 
 void setup() {
   pinMode(HUMID_PIN, INPUT_PULLUP);
@@ -38,28 +34,11 @@ void setup() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 
-  char buf[ID.length()];
-  ID.toCharArray(buf, ID.length());   //convert ID from String to char*
-  data = buf;                         //since udp.write can't take String
+  char data[ID.length()];
+  ID.toCharArray(data, ID.length());   //convert ID from String to char*
 }
 
 void loop() {
-  //Method using Strings
-  /*String humidity = String(analogRead(HUMID_PIN));
-  String light    = String(analogRead(LIGHT_PIN));
-  json += "{\"ID\":";
-  json += ID;
-  json += ",\"humidity\":";
-  json += humidity;
-  json += ",\"lightExposure\":";
-  json += light;
-  json += "}";
-
-  char buf[json.length()];
-  json.toCharArray(buf, json.length());
-  char* data = buf;*/
-
-  //Method using char*
   int a = analogRead(HUMID_PIN);
   char* humidity;
   itoa(a, humidity, 10);  //convert int to char*, base 10
